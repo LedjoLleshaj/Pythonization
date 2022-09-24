c87f3e9cdea7755fa32d5717af16c291bdc27e89
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-09-24 19:51:44 +0200

    Division stuff how bout dat

---
+++ 03-Numbers-Booleans-and-Equality/division-floor-division-and-the-modulo-operator.py
@@ -0,0 +1,6 @@
+print(15/3) #prints 5.0
+print(15//3) #prints 5 floor division eliminates the decimal
+print(15%3) #prints 0 
+print(15%4) #prints 3 
+print(-14//3) #prints -5
+print(-14%3) #prints 2
