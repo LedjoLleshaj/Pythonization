e080518b41dc46622d838db6a2a299d66d302697
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-09-24 19:34:00 +0200

    Pretty basic stuff

---
+++ 03-Numbers-Booleans-and-Equality/mathematical-expressions.py
@@ -0,0 +1,11 @@
+print(3+4)
+print(5.5-4.5)
+print(3*4)
+
+print("Bac"+"on")
+print("Bac"*3) #prints "BacBacBac"
+
+print(2**5) #prints 32
+
+print(3+5*4) #prints 23  right to left order of operations
+print((3+5)*4) #prints 32
