020b3da2db907f11465984d8e45628ac9eb34723
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-09-24 17:43:49 +0200

    These strings hella long

---
+++ 02-Welcome-to-Python/objects-strings.py
@@ -0,0 +1,7 @@
+"sushi"
+'sushi'
+"'sushi'"
+print("Damm this guy really likes sushi")
+print('This "Pythonization" thing is a great way to learn Python')
+print( """This is a multi-line string
+that spans multiple lines""")
