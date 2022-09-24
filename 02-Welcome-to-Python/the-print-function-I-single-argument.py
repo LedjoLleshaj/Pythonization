e24f7fb950796c9ad02e71abad4b7d8bd31a13e1
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-09-24 17:52:36 +0200

    Printed some selfaware strings i guess

---
+++ 02-Welcome-to-Python/the-print-function-I-single-argument.py
@@ -0,0 +1,5 @@
+print("Let's print some worthless stuff")
+print("Ye pretty worthless this stuff")
+print("I'm not even sure why I'm doing this")
+print("I guess I'm just trying to fill up space")
+print("I'm not even sure if this is a good idea")
