088d610ceebca14d2cfed152189f437408030f11
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-09-24 20:03:41 +0200

    Bool this python!!

---
+++ 03-Numbers-Booleans-and-Equality/the-boolean-data-type.py
@@ -0,0 +1,35 @@
+print(True) # True
+print(False) # False
+
+print(1 == 1) # True
+print(1 == 2) # False
+
+print(1 != 1) # False
+print(1 != 2) # True
+
+print(1 > 2) # False
+print(1 < 2) # True
+
+print(1 >= 2) # False
+print(1 <= 2) # True
+
+print(1 > 1) # False
+print(1 >= 1) # True
+
+print("Hello" == "Hello") # True
+print("Hello" == "hello") # False
+
+print("Hello" != "Hello") # False
+print("Hello" != "hello") # True
+
+print(5 == 5.0) # True
+print(5 == 5.1) # False
+print(5 != 5.0) # False
+print(5 != 5.1) # True
+
+print(True == 1) # True
+print(False == 0) # True
+
+print(True != 1) # False
+print(False != 0) # False
+
