8e178c76bffbfa3eaacf96091ebf5ecd58a8fe7d
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-09-25 19:15:57 +0200

    how about i vary this variable

---
+++ 04-Variables/intro-to-variables.py
@@ -0,0 +1,9 @@
+name = "Ledjo"
+age = 22
+handsome = True
+snake_case_notation = True
+
+print(name)
+print(age - 4) #hehe
+print(handsome) #no need to even print this but ok
+print(snake_case_notation)
