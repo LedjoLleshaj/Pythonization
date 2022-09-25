b9e1f5d1cb89946b41d3b765810e91e568fcb3a6
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-09-25 20:46:25 +0200

    Errors, my future bestfriend

---
+++ 04-Variables/the-NameError-ValueError-TypeError-and-SyntaxError-Exceptions.py
@@ -0,0 +1,11 @@
+#first = "Ledjo"
+
+# print(fist) # NameError: name 'fist' is not defined
+
+# print(1 + first) # TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+# print(first + 1) # TypeError: can only concatenate str (not "int") to str
+
+# int(first) # ValueError: invalid literal for int() with base 10: 'xyz'
+
+# print("Hello, World!" # SyntaxError: invalid syntax
