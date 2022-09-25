03820e7f7976eb9394059a01c8a1ba2759f0b3a0
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-09-25 21:25:59 +0200

    My favorite type

---
+++ 05-Functions/the-none-type.py
@@ -0,0 +1,11 @@
+a = None
+print(a)
+print(type(a))
+
+def my_function():
+    print("Hello from a function")
+
+result = my_function()
+print(result)
+print(type(result))
+print(type(my_function()))
