60d23062148b8c86d1c9d20467934583421713f2
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-09-25 22:18:34 +0200

    Slicing my String of

---
+++ 06-Strings-The-Basics/string-slicing-I-slicing-by-range.py
@@ -0,0 +1,10 @@
+address = "Street 1, City 12, Country 123"
+print(address[0:6]) # Street Don't forget that the last index is not included
+print(address[10:14]) # City 
+print(address[19:-3]) # Country
+print(address[0:100]) # Street 1, City 12, Country 123
+
+print(address[5:]) # starts at index 5 and goes to the end
+print(address[:5]) # starts at the beginning and goes to index 5
+print(address[:]) # starts at the beginning and goes to the end
+
