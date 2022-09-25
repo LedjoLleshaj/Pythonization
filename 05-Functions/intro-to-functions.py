deaad13c94bea5f6b5ace74f23d49649ad1f4dc4
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-09-25 21:00:45 +0200

    Functionins

---
+++ 05-Functions/intro-to-functions.py
@@ -0,0 +1,8 @@
+def output_text():
+    print("This is a function")
+    print("Functions need to idented")
+    print("Functions need to be called to run")
+    
+
+output_text()
+# output_text(2) # This will cause a TypeError
