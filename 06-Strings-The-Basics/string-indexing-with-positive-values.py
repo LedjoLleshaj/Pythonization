48dddf01e569356125296307b0e6e6b186bb483f
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-09-25 22:01:58 +0200

    Positive indexes

---
+++ 06-Strings-The-Basics/string-indexing-with-positive-values.py
@@ -0,0 +1,10 @@
+str = "Python"
+print(str[0]) # P
+print(str[1]) # y
+print(str[2]) # t
+print(str[3]) # h
+print(str[4]) # o
+print(str[5]) # n
+# print(str[6]) # IndexError: string index out of range
+print(type(str[0])) # <class 'str'>
+str[0] = "p" # TypeError: 'str' object does not support item assignment
