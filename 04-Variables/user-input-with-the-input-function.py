58d51a4e9982b99598f0ef86cb1c584ea4c3484c
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-09-25 19:48:52 +0200

    Get that 'in put' baby

---
+++ 04-Variables/user-input-with-the-input-function.py
@@ -0,0 +1,9 @@
+inp = input("Write something biatch: ") #input gets input and returns as String
+print(inp)
+print("Finished")
+
+print("Let me help you add two numbers")
+num = int(input("First input "))
+second = int(input("Second input "))
+print("This is the sum dickhead: ",num + second, "has type: ",type(num))
+
