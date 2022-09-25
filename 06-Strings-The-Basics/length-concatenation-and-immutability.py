60bd55741a1ee7c86ee955c38ccde6d14584f5fa
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-09-25 21:55:41 +0200

    String basic operations

---
+++ 06-Strings-The-Basics/length-concatenation-and-immutability.py
@@ -0,0 +1,11 @@
+name = "Ledjo"
+sirname = "Lleshaj"
+print(len(name)) # 5
+print(len(sirname)) # 8
+print(len(name + sirname)) # 13
+print(name + sirname) # LedjoLleshaj
+print(name + " " + sirname) # Ledjo Lleshaj
+print(name + " " + sirname + "!") # Ledjo Lleshaj!
+
+# name[0] = "l" # TypeError: 'str' object does not support item assignment Immutable
+print(name[0]) # L
