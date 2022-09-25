4655144e018e938d69212621e56ae05c8d205a01
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-09-25 19:32:00 +0200

    Hmm nice

---
+++ 04-Variables/multiple-variable-assignments.py
@@ -0,0 +1,13 @@
+a = 5
+b = 5
+print(a)
+print(b)
+
+a = b = 10 #this is called multiple variable assignment and is not recommended  #because it is confusing
+a, b = 10, 20 #this is also called multiple variable assignment and is not recommended  #because it is confusing
+
+print(a)
+print(b)
+b =5
+print(a)
+print(b)
