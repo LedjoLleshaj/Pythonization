00d8a59854b95ddf3f8175144f738762e709f26e
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-10-07 20:20:34 +0200

    Im gonna capitalize on these functions

---
+++ 07-Strings-Methods/the-capitalize-title-lower-upper-and-swapcase-methods.py
@@ -0,0 +1,18 @@
+story = "once upon a time in a land far, far away..."
+
+# Capitalize the first letter of the string
+print(story.capitalize()) # Once upon a time in a land far, far away...
+
+# Title case the string 
+print(story.title()) # Once Upon A Time In A Land Far, Far Away...
+
+# Convert the string to lowercase
+print(story.lower()) # once upon a time in a land far, far away... 
+
+# Convert the string to uppercase
+print(story.upper()) # ONCE UPON A TIME IN A LAND FAR, FAR AWAY...
+
+# Swap the case of the string
+print(story.swapcase()) # ONCE UPON A TIME IN A LAND FAR, FAR AWAY...  
+
+print(story) #string is unchanged becaue strings are immutable in python
