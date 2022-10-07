0920aa016b8bb0f4bd817d1cf309818f47e06b7c
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-10-07 22:02:50 +0200

    Formatting string literals. noice stuff

---
+++ 07-Strings-Methods/formatted-string-literals.py
@@ -0,0 +1,7 @@
+name = "Ledjo"
+adjective = "funny"
+noun = "human"
+
+# formatted string literals (f-strings) are a new way to format strings in Python 3.6+
+mad_libs = f"{name} laughed at the {adjective} {noun}." # Ledjo laughed at the funny human. 
+print(f"2 + 2 = {2 + 2}") # 2 + 2 = 4
