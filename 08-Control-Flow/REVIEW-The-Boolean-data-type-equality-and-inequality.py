b3a63b230df29be57414f3cc4f0ce77e6613b4ff
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-10-12 17:50:43 +0200

    More boolean stuff

---
+++ 08-Control-Flow/REVIEW-The-Boolean-data-type-equality-and-inequality.py
@@ -0,0 +1,13 @@
+from operator import truediv
+
+
+handsome = true
+admin = false
+print(2 < 3) # True
+print(2 > 3) # False
+print(2 == 3) # False
+print(2 != 3) # True
+print("yes" == "yes") # True
+print("yes" == "Yes") # False
+
+
