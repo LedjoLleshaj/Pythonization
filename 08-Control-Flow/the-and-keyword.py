4bf60a175b86a342826a91b885ca07ff798800d9
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-10-12 18:40:34 +0200

    And EZ

---
+++ 08-Control-Flow/the-and-keyword.py
@@ -0,0 +1,9 @@
+if 5 < 7 and "a" == "a":
+    print("Both statements are true")
+
+if 5 < 7 and "a" == "b":
+    print("This will not print because the second statement is false")
+
+if 5 > 7 and "a" == "a" : #python wll actually check only the first condition and stop
+    print("This will not print because the first statement is false")
+
