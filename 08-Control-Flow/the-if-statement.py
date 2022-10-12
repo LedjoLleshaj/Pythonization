79de8cce5fbeec0ff2a7361ddd5a3085da3bd433
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-10-12 17:57:51 +0200

    If this if that

--- 08-Control-Flow/the-if-statement.py
+++ 08-Control-Flow/the-if-statement.py
@@ -1,2 +1,22 @@
 if 5 > 3 :
     print("5 is greater than 3")
+elif 5 < 3 :
+    print("5 is less than 3")
+else :
+    print("5 is equal to 3")
+
+if "yes" == "yes" :
+    print("yes is equal to yes")
+else :
+    print("yes is not equal to yes")
+
+if "yes" == "Yes" :
+    print("yes is equal to Yes")
+else :
+    print("yes is not equal to Yes")
+
+if True :
+    print("This will always print")
+
+if False :
+    print("This will never print")
