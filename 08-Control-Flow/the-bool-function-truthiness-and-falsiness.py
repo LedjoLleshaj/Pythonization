e80f792bab40f571e189dd8d3ae0d0cdae7519fb
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-10-12 18:17:46 +0200

    Bool stuf

---
+++ 08-Control-Flow/the-bool-function-truthiness-and-falsiness.py
@@ -0,0 +1,20 @@
+if 3:
+    print("3 will always print")
+
+if 0:
+    print("0 will never print")
+
+if "yes":
+    print("yes will always print")
+
+if -1:
+    print("-1 will always print")
+
+if "":
+    print(" will never print")
+
+print(bool(3))
+print(bool(0))
+print(bool("yes"))
+print(bool(-1))
+print(bool(""))
