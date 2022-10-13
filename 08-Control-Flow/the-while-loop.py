c68e7a8c773753bed43846add8696e29a1e41de7
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-10-13 18:18:01 +0200

    Eternal loop

--- 08-Control-Flow/the-while-loop.py
+++ 08-Control-Flow/the-while-loop.py
@@ -6,13 +6,28 @@
  
 # print(count) # 5
 
+# invalid_number = True
 
-invalid_number = True
+# while invalid_number:
+#     number = input("Enter a number between 1 and 10: ")
+#     if number.isdigit() and 1 <= int(number) <= 10:
+#         invalid_number = False
+#         print(f"Well done! Number {number} is great.")
+#     else:
+#         print("Invalid number.Try again!")
 
-while invalid_number:
-    number = input("Enter a number between 1 and 10: ")
-    if number.isdigit() and 1 <= int(number) <= 10:
-        invalid_number = False
-        print(f"Well done! Number {number} is great.")
+def fizz_buzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0 and number % 5 != 0:
+        print("Fizz")
+    elif number % 5 == 0 and number % 3 != 0:
+        print("Buzz")
     else:
-        print("Invalid number.Try again!")
+        print(number)
+
+number = input("Enter a number: ")
+count = 1
+while count <= int(number): 
+    fizz_buzz(count)
+    count += 1
