8fe338a1c8b53ac35941a13b54f4c5e3d6dd9f71
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-10-13 19:24:27 +0200

    To loop is human to recure is Divine

---
+++ 08-Control-Flow/a-brief-intro-to-recursion-II.py
@@ -0,0 +1,19 @@
+def reverse_iterative(s):
+    count = len(s)
+    result = ""
+    while count > 0:
+        result += s[count-1]
+        count -= 1
+    return result
+
+reversed = reverse_iterative("hello")
+print(f"The reverse of 'hello' is {reversed}.") # olleh
+
+def reverse_recursive(s):
+    print(s)
+    if len(s) == 1:
+        return s
+    return reverse_recursive(s[1:]) + s[0]
+
+reversed = reverse_recursive("hello")
+print(f"The reverse of 'hello' is {reversed}.") # olleh
