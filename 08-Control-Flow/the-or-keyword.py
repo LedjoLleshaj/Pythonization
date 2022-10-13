90e91fd8abe9c7fd1a07967419e5b2772c372c00
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2022-10-13 16:45:05 +0200

    Or conditions pretty boring Morgan

---
+++ 08-Control-Flow/the-or-keyword.py
@@ -0,0 +1,8 @@
+if 5 > 8 or 3 > 2:
+    print("At least one of the statements is true")
+
+if "a" > "b" or 3 < 2:
+    print("At least one of the statements is true")
+
+if "cat" == "cat" or "dog" == "dawg":
+    print("At least one of the statements is true")
