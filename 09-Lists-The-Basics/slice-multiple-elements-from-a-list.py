2f125f3709010d5e41a13255363a94cd6ed42d41
ledjo <ledjolleshaj@yahoo.com>
2022-11-02 16:35:27 +0100

    Slice that list

---
+++ 09-Lists-The-Basics/slice-multiple-elements-from-a-list.py
@@ -0,0 +1,13 @@
+print("programming"[3:6]) # prints "gra"
+
+muscles = ["biceps", "triceps", "delts", "pecs"]
+print(muscles[1:3]) # doesn't include the upper bound element in the list
+
+print(muscles[0:2]) #print(muscles[:2])
+print(muscles[2:5]) 
+print(muscles[2:])
+print(muscles[-3:])
+
+print(muscles[1:-1])
+
+print(muscles[::-1])
