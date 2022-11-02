1891ab6cc80d02e388d140dad7f6ce61ba3e8011
ledjo <ledjolleshaj@yahoo.com>
2022-11-02 10:09:05 +0100

    Positive a negative indexes on lists

---
+++ 09-Lists-The-Basics/select-a-list-element-by-positive-or-negative-index-positions.py
@@ -0,0 +1,14 @@
+print("organic"[5]) # c
+#we can use the index position for lists also
+web_browser = ["Chrome", "Firefox", "Safari", "Edge"]
+print(web_browser[0]) # Chrome
+print(web_browser[1]) # Firefox
+# print(web_browser[5]) # IndexError: list index out of range
+
+print(web_browser[2][0]) # S
+
+presidents = ["Washington", "Adams", "Jefferson"]
+print(presidents[-1]) # Jefferson
+print(presidents[-2]) # Adams
+print(presidents[-3]) # Washington
+print(presidents[:-1]) # ['Washington', 'Adams']
