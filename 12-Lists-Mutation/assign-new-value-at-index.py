1733e2dc4e95d06f955dd904326d8a279dfe70a6
ledjo <ledjolleshaj@yahoo.com>
2022-11-03 17:16:22 +0100

    Mutating lists

---
+++ 12-Lists-Mutation/assign-new-value-at-index.py
@@ -0,0 +1,9 @@
+crayons = ["Macaroni and Cheese", "MAximum yellow Red", "Jazzberry Jam"]
+print(crayons)
+
+crayons[0] = "Aquamarine" # this can be done since the list is mutable
+crayons[-1] = "Cotton Candy" 
+print(crayons)
+
+
+
