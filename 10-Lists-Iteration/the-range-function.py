d2ba102f41d24ecaaa2095cb4c0178e3792bba0a
ledjo <ledjolleshaj@yahoo.com>
2022-11-03 15:16:49 +0100

    Range function

---
+++ 10-Lists-Iteration/the-range-function.py
@@ -0,0 +1,15 @@
+#range returns a list of numbers from 0 to the number you pass in
+# it is the type of list that is called a generator
+# it is a list that is not stored in memory
+
+for number in range(5): # 0,1,2,3,4
+    print(number)
+
+for number in range(3, 5): # 3,4
+    print(number)
+
+for number in range(0, 10, 2): # 0,2,4,6,8
+    print(number)
+
+ 
+
