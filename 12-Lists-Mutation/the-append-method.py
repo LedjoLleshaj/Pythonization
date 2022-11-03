c74acb57a704756bb627726a786bae8460ada800
ledjo <ledjolleshaj@yahoo.com>
2022-11-03 17:36:29 +0100

    Append function

---
+++ 12-Lists-Mutation/the-append-method.py
@@ -0,0 +1,7 @@
+countries = [ 'India', 'USA', 'UK', 'Japan', 'China' ]
+
+countries.append('Russia')
+
+print(countries)
+# Output: ['India', 'USA', 'UK', 'Japan', 'China', 'Russia']
+# append() method adds an element to the end of the list.
