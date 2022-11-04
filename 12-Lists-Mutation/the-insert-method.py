e5c8dfee0323c6d9d2b6786bb069a8066fd2ce16
ledjo <ledjolleshaj@yahoo.com>
2022-11-04 15:09:46 +0100

    Insert element in a give index...noice

---
+++ 12-Lists-Mutation/the-insert-method.py
@@ -0,0 +1,13 @@
+plays = ["Hamlet", "Macbeth", "King Lear"]
+
+plays.insert(1, "Romeo and Juliet")
+# The insert() method inserts an element to the list at the specified index.
+# The insert() method doesn't have to return any value
+# The insert() method has two parameters:
+# 1. index - where the element should be inserted. If the index is 0, the element will be inserted at the first position.
+# 2. element - the element to be inserted
+# If the index is greater than the length of the list, the element will be inserted at the end of the list.
+# If the index is a negative number, the element will be inserted at the end of the list, starting from the end.
+# It Will not replace any element, but it will insert the element at the specified index.
+# The insert() method returns None
+
