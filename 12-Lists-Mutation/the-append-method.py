3dfcadbba5d8919d8183390d9e2cf4ef8d7845e3
ledjo <ledjolleshaj@yahoo.com>
2022-11-04 14:29:30 +0100

    Append this buddy

--- 12-Lists-Mutation/the-append-method.py
+++ 12-Lists-Mutation/the-append-method.py
@@ -5,3 +5,6 @@ countries.append('Russia')
 print(countries)
 # Output: ['India', 'USA', 'UK', 'Japan', 'China', 'Russia']
 # append() method adds an element to the end of the list.
+# append() method doesn't return any value (returns None).
+# append() method only accepts a single argument.
+
