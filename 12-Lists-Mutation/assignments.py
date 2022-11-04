c57a95d1314aadd1954fcbdd2a5ac2c39e0ec49a
ledjo <ledjolleshaj@yahoo.com>
2022-11-04 16:26:19 +0100

    Done assignments

--- 12-Lists-Mutation/assignments.py
+++ 12-Lists-Mutation/assignments.py
@@ -19,3 +19,140 @@ tech_companies = ["Google", "Microsoft", "Blackberry", "IBM", "Yahoo"]
 
 tech_companies[1:4] = ["Facebook" , "Apple"]
 
+# Declare a same_index_values function that accepts two lists.
+# The function should return a list of the index positions in which the two lists have equal elements
+#
+# EXAMPLES
+# same_index_values([1, 2, 3], [3, 2, 1])                         => [1]
+# same_index_values(["a", "b", "c", "d"], ["c", "b", "a", "d"])   => [1, 3]
+
+def same_index_values(list1,list2):
+    res = []
+    for index,el in enumerate(list1):
+        if list2[index] == el:
+            res.append(index)
+            
+    return res
+
+# Declare a same_index_values function that accepts two lists.
+# The function should return a list of the index positions in which the two lists have equal elements
+#
+# EXAMPLES
+# same_index_values([1, 2, 3], [3, 2, 1])                         => [1]
+# same_index_values(["a", "b", "c", "d"], ["c", "b", "a", "d"])   => [1, 3]
+
+def same_index_values(list1,list2):
+    res = []
+    #not complete because i would need to check which list is longer.
+    #or if i have empty lists or lists
+    for index,el in enumerate(list1):
+        if list2[index] == el:
+            res.append(index)
+            
+    return res
+
+
+# Declare a sum_from function that accepts two numbers as arguments.
+# The second number will always be greater than the first number.
+# The function should return the sum of all numbers from the first number to the second number (inclusive).
+#
+# EXAMPLES
+# sum_from(1, 2)   # 1 + 2                  => 3
+# sum_from(1, 5)   # 1 + 2 + 3 + 4 + 5      => 15
+# sum_from(3, 8)   # 3 + 4 + 5 + 6 + 7 + 8  => 33
+# sum_from(9, 12)  # 9 + 10 + 11 + 12       => 42
+
+def sum_from(num1,num2):
+    total = 0
+    for val in range(num1,num2+1):#end index is exclusive
+        total +=val
+        
+    return total
+
+# Define a long_strings function that accepts a list of strings. 
+# It should return a new list consisting of only the strings that have 5 characters or more.
+#
+# EXAMPLES
+# long_strings(["Hello", "Goodbye", "Sam"])  => ["Hello", "Goodbye"]
+# long_strings(["Ace", "Cat", "Job"])        => []
+# long_strings([])                           => []
+
+def long_strings(list):
+    res = []
+    for el in list:
+        if len(el) >= 5 :
+            res.append(el)
+    
+    return res
+
+# Define an only_evens function that accepts a list of numbers. 
+# It should return a new list consisting of only the even numbers from the original list.
+#
+# EXAMPLES
+# only_evens([4, 8, 15, 16, 23, 42]) => [4, 8, 16, 42]
+# only_evens([1, 3, 5])              => []
+# only_evens([])                     => []
+
+def only_evens(list):
+    res = []
+    for el in list:
+        if el % 2 == 0:
+            res.append(el)
+    
+    return res
+ 
+ # Write a factors function that accepts a positive whole number
+# It should return a list of all of the number's factors in ascending order
+# HINT: Could the range function be helpful here? Or maybe a while loop?
+#
+# EXAMPLES
+# factors(1)  => [1]
+# factors(2)  => [1, 2]
+# factors(10) => [1, 2, 5, 10]
+# factors(64) => [1, 2, 4, 8, 16, 32, 64]
+
+def factors(num):
+    res = []
+    for factor in range(1,num+1):
+        if num % factor == 0:
+            res.append(factor)
+            
+    return res
+
+# Declare a push_or_pop function that accepts a list of numbers
+# Build up and return a new list by iterating over the list of numbers
+# If a number is greater than 5, add it to the end of the new list
+# If a number is less than or equal to 5, remove the last element added to the new list
+# Assume the order of numbers in the argument will never require removing from an empty list
+#
+# EXAMPLES
+# push_or_pop([10])            => [10]
+# push_or_pop([10, 4])         => []
+# push_or_pop([10, 20, 30])    => [10, 20, 30]
+# push_or_pop([10, 20, 2, 30]) => [10, 30]
+
+def push_or_pop(list):
+    res = []
+    for el in list:
+        if el > 5:
+            res.append(el)
+        else:
+            res.pop()
+            
+    return res
+
+
+# Declare a delete_all function that accepts a list of strings and a target string
+# Remove all occurrences of the target string from the list and return it
+#
+# EXAMPLES
+# delete_all([1, 3, 5], 3)  => [1, 5]
+# delete_all([5, 3, 5], 5)  => [3]
+# delete_all([4, 4, 4], 4)  => []
+# delete_all([4, 4, 4], 6)  => [4, 4, 4]
+
+def delete_all(list,target):
+    while(target in list):
+        list.remove(target)
+        
+    return list
