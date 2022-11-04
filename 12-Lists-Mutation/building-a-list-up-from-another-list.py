6870fd8cad2bdf2f5db7a677b0390ab42bf94243
ledjo <ledjolleshaj@yahoo.com>
2022-11-04 15:00:28 +0100

    EZ

--- 12-Lists-Mutation/building-a-list-up-from-another-list.py
+++ 12-Lists-Mutation/building-a-list-up-from-another-list.py
@@ -6,8 +6,8 @@ def squares(numbers):
         res.append(num**2)
     return res
 
-print(powerball_numbers)
-print(squares(powerball_numbers))
+print(powerball_numbers) # [4, 8, 15, 16, 23, 42]
+print(squares(powerball_numbers)) # [16, 64, 225, 256, 529, 1764]
 
 def convert_to_floats(numbers):
     res = []
@@ -15,4 +15,4 @@ def convert_to_floats(numbers):
         res.append(float(num))
     return res
 
-print(convert_to_floats(powerball_numbers))
+print(convert_to_floats(powerball_numbers)) # [4.0, 8.0, 15.0, 16.0, 23.0, 42.0]
