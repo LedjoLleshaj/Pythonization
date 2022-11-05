1eac92e2514130d79866a62784d81b192cc2998b
ledjo <ledjolleshaj@yahoo.com>
2022-11-05 11:26:23 +0100

    Can't beat em? Join em

---
+++ 13-Lists-Methods/the-join-method-on-a-string.py
@@ -0,0 +1,13 @@
+address = ["123 Main Street", "Anytown", "CA", "12345"]
+
+print(", ".join(address)) # 123 Main Street, Anytown, CA, 12345
+
+#join method on a string or list
+#the join method on a string or list returns a string that is the concatenation of the elements of the list
+#the elements of the list are separated by the string that the method is called on
+#the elements of the list are not changed
+#the elements of the list must be strings
+#the elements of the list can be separated by any string
+#the elements of the list can be separated by a space, a comma, a period, a dash, a slash, etc.
+
+
