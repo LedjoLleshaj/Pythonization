e1e61078b164a2342f97a877da17ddd4b93cd1ac
ledjo <ledjolleshaj@yahoo.com>
2022-11-10 16:08:06 +0100

    added Filter explanation

--- 14-Built-in-Functions/the-filter-function.py
+++ 14-Built-in-Functions/the-filter-function.py
@@ -1,5 +1,10 @@
 animals = ["cat", "dog", "fish", "bird"]
 
+
+#for fiter function to work, it needs a function and a list
+#filter function will return a list of elements that have verify the condition
+#in this case, it will return a list of animals that are longer than 3 characters
+
 long_words = [animal for animal in animals if len(animal) > 3]
 print(long_words)
 long_words = list(filter(is_long_animal, animals))
