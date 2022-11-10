6122a5671b5ae4f24beba270c3068860c7ba9011
ledjo <ledjolleshaj@yahoo.com>
2022-11-10 15:27:01 +0100

    Help built-in function...all hail google

---
+++ 14-Built-in-Functions/the-help-function.py
@@ -0,0 +1,10 @@
+help(len)
+help(print)
+help(help)
+# help is a built-in function that displays the documentation for a function
+# or module. It can be used to display the documentation for any function or
+# we can also use it for classes.
+
+help("Hello".replace)
+# help can also be used to display the documentation for a method of an object.
+
