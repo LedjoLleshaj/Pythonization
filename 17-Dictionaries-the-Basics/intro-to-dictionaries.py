9e46ba3b0569798ffd4c1116516d543daf8a2b98
ledjo <ledjolleshaj@yahoo.com>
2022-11-14 09:25:57 +0100

    Dictionaries

---
+++ 17-Dictionaries-the-Basics/intro-to-dictionaries.py
@@ -0,0 +1,19 @@
+{} # its an empty dictionary
+{'key': 'value'} # its a dictionary with one key-value pair
+{'key1': 'value1', 'key2': 'value2'} # its a dictionary with two key-value pairs
+#  a dictionary is a collection of key-value pairs
+#  a key is a string
+#  a value can be any data type
+#  a key-value pair is separated by a colon
+#  each key-value pair is separated by a comma
+#  a dictionary is enclosed in curly braces
+#  a dictionary is unordered
+#  a dictionary is mutable
+#  a dictionary is indexed by keys
+ice_cream_preferences = {
+    'John': 'vanilla',
+    'Mary': 'chocolate', 
+    'Bob': 'strawberry'
+}
+print(ice_cream_preferences) # {'John': 'vanilla', 'Mary': 'chocolate', 'Bob': 'strawberry'}
+print(len(ice_cream_preferences)) # 3
