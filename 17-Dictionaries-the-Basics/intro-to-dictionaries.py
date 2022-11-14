376cd78f0604d8828d5b30af4cf5b6ad25ed116a
ledjo <ledjolleshaj@yahoo.com>
2022-11-14 09:29:52 +0100

    Dictionaries

--- 17-Dictionaries-the-Basics/intro-to-dictionaries.py
+++ 17-Dictionaries-the-Basics/intro-to-dictionaries.py
@@ -2,7 +2,6 @@
 {'key': 'value'} # its a dictionary with one key-value pair
 {'key1': 'value1', 'key2': 'value2'} # its a dictionary with two key-value pairs
 #  a dictionary is a collection of key-value pairs
-#  a key is a string
 #  a value can be any data type
 #  a key-value pair is separated by a colon
 #  each key-value pair is separated by a comma
