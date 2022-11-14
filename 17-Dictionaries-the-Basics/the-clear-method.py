195d427f872e70b27687f42c62f78811b0a0beeb
ledjo <ledjolleshaj@yahoo.com>
2022-11-14 11:36:01 +0100

    Clean plate.. akhem i mean dictionary

---
+++ 17-Dictionaries-the-Basics/the-clear-method.py
@@ -0,0 +1,7 @@
+websites = {
+    "Google": "https://www.google.com",
+    "Facebook": "https://www.facebook.com",
+    "Twitter": "https://www.twitter.com",
+}
+websites.clear()
+print(websites) # {} because clear() method removes all items from the dictionary
