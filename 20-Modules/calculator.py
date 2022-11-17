d7fb84d68017a2569ce10b6fd36f9c98a4ba241e
ledjo <ledjolleshaj@yahoo.com>
2022-11-17 16:57:26 +0100

    First calculator module Oh Wow!

---
+++ 20-Modules/calculator.py
@@ -0,0 +1,23 @@
+# modules in python are just python files
+# we can import them and use the functions defined in them
+# we can import the whole module
+
+
+creator = "Ledjo Lleshaj"
+PI = 3.141592653589793
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+def area_of_circle(r):
+    return PI * r * r
+
+print(add(1, 2))
---
+++ 20-Modules/my_program.py
@@ -0,0 +1,15 @@
+# inpython everythig is an object
+# so also modules are objects
+# we can see the attributes of a module
+
+import calculator #i can import  only files in the same directory
+# if a file is in a different directory i have to specify the path
+
+
+print(calculator.creator)
+print(calculator.PI)
+print(calculator.add(1, 2))
+print(calculator.subtract(1, 2))
+print(calculator.multiply(1, 2))
+print(calculator.divide(1, 2))
+print(calculator.area_of_circle(1))
