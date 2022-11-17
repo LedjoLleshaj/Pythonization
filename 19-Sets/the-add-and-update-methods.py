b1be18a931ab9921c347bdaf161ac06333709ad8
ledjo <ledjolleshaj@yahoo.com>
2022-11-17 16:04:44 +0100

    EZ

---
+++ 19-Sets/the-add-and-update-methods.py
@@ -0,0 +1,18 @@
+disney_characters = {
+    "Mickey Mouse",
+    "Donald Duck",
+    "Goofy"
+}
+disney_characters.add("Minnie Mouse")
+disney_characters.add("Minnie Mouse")
+print(disney_characters)
+
+disney_update = {
+    "Mickey Mouse",
+    "Donald Duck",
+    "Superman",
+    "Batman"
+}
+disney_characters.update(disney_update)
+print(disney_characters)
+
