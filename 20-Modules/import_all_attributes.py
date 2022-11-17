9bce8ccd6bf9c9ce5fc201e42864b05ad84ec9fb
ledjo <ledjolleshaj@yahoo.com>
2022-11-17 18:21:00 +0100

    Name collision bs still

---
+++ 20-Modules/import_all_attributes.py
@@ -0,0 +1,5 @@
+from calculator import *
+# now we have imported all the functions from the calculator module
+# we can use them without the module name. Risk of name collisions
+print(creator)
+print(add(1,2))
