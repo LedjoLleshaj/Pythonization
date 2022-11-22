ec6c8b49aff1ad619414d9ae5c8b40c0277bbc52
ledjo <ledjolleshaj@yahoo.com>
2022-11-22 19:08:46 +0100

    Old java memories comming back

---
+++ 23-Classes-the-Basics/class-definition-and-instantiation.py
@@ -0,0 +1,14 @@
+# classes are defined using the keyword class and respect the PascalCase convention
+class Person():
+    pass
+
+class DatabaseConnection():
+    pass
+
+# classes are instantiated using the class name as a function
+Ledjo = Person()
+db = DatabaseConnection()
+print(type(Ledjo))
+print(type(db))
+
+
