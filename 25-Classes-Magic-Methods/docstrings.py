cd7d9ab4328bc4d84e6b0b9618deffaca15b852f
ledjo <ledjolleshaj@yahoo.com>
2022-11-28 19:04:14 +0100

    Use help over print for docstrings

---
+++ 25-Classes-Magic-Methods/docstrings.py
@@ -0,0 +1,15 @@
+# docstrings are used to document the code and are used by the help() function
+# to display the documentation of the function
+# docstrings are enclosed in triple quotes
+# docstrings are optional but highly recommended
+import sushi
+import math
+# print(sushi.__doc__) # prints the docstring of the module
+# print(sushi.fish.__doc__) # prints the docstring of the function
+# print(sushi.Salmon.__doc__) # prints the docstring of the class
+# print(sushi.Salmon.__init__.__doc__) # prints the docstring of the constructor
+# print(sushi.Salmon.bake.__doc__) # prints the docstring of the method
+
+# print(math.__doc__)
+
+help(sushi) # help is preferred over printing each docstring
---
+++ 25-Classes-Magic-Methods/sushi.py
@@ -0,0 +1,26 @@
+"""
+A completely normal module here.
+No fishy code here.
+"""
+def fish():
+    """
+    This function returns True
+    """
+    return True
+
+class Salmon():
+    """
+    This class is a Salmon class
+    """
+    def __init__(self):
+        """
+        This is the constructor of the Salmon class
+        """
+        self.taste = 10
+
+    def bake(self):
+        """
+        This function bakes the salmon
+        """
+        self.taste += 1
+
