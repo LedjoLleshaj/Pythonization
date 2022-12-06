262c14d5446a764428978cb47e71a60695620ac0
ledjo <ledjolleshaj@yahoo.com>
2022-12-06 13:39:27 +0100

    Basic stuff

---
+++ 29-The-random-Module/the-random-randint-and-randrange-functions.py
@@ -0,0 +1,9 @@
+import random
+
+print(random.random)
+
+print(random.randint(1, 10))  # possible values: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+print(
+    random.randrange(0, 50, 5)
+)  # possible values: 0, 5, 10, 15, 20, 25, 30, 35, 40, 45
