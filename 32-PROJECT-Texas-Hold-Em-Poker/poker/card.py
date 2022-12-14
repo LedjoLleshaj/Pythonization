5db503809e9b764e05f560f0865739ca9cbd649c
ledjo <ledjolleshaj@yahoo.com>
2022-12-14 16:27:18 +0100

    First steps to glory

--- 32-PROJECT-Texas-Hold-Em-Poker/poker/card.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/poker/card.py
@@ -1,4 +1,32 @@
 class Card:
+    SUITS = ["Spades", "Clubs", "Diamonds", "Hearts"]
+    RANKS = [
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "Jack",
+        "Queen",
+        "King",
+        "Ace",
+    ]
+
     def __init__(self, rank, suit):
+        if rank not in self.RANKS:
+            raise ValueError(f"Invalid rank {rank}")
+        if suit not in self.SUITS:
+            raise ValueError(f"Invalid suit {suit}")
+
         self.rank = rank
         self.suit = suit
+
+    def __str__(self):
+        return f"{self.rank} of {self.suit}"
+
+    def __repr__(self):
+        return f"Card('{self.rank}', '{self.suit}')"
--- 32-PROJECT-Texas-Hold-Em-Poker/tests/test_card.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/tests/test_card.py
@@ -8,5 +8,44 @@ class CardTest(unittest.TestCase):
         self.assertEqual(card.rank, "Queen")
 
     def test_has_suit(self):
-        card = Card(rank="Ace", suit="Spade")
-        self.assertEqual(card.suit, "Spade")
+        card = Card(rank="Ace", suit="Spades")
+        self.assertEqual(card.suit, "Spades")
+
+    def test_has_string_representation(self):
+        card = Card(rank="Ace", suit="Spades")
+        self.assertEqual(str(card), "Ace of Spades")
+
+    def tests_has_technical_representation(self):
+        card = Card(rank="Ace", suit="Spades")
+        self.assertEqual(repr(card), "Card('Ace', 'Spades')")
+
+    def test_card_has_four_possible_suits(self):
+        self.assertEqual(Card.SUITS, ["Spades", "Clubs", "Diamonds", "Hearts"])
+
+    def test_card_has_13_possible_ranks(self):
+        self.assertEqual(
+            Card.RANKS,
+            [
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                "10",
+                "Jack",
+                "Queen",
+                "King",
+                "Ace",
+            ],
+        )
+
+    def test_card_only_accepts_valid_ranks(self):
+        with self.assertRaises(ValueError):
+            Card(rank="Two", suit="Spades")
+
+    def test_card_only_accepts_valid_suits(self):
+        with self.assertRaises(ValueError):
+            Card(rank="Ace", suit="yolo")
