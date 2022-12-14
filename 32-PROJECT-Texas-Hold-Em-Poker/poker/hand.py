4751d8faa9c40bee326a78fe4e77bb9d4dc3be24
ledjo <ledjolleshaj@yahoo.com>
2022-12-14 18:38:04 +0100

    Check for pairs(not complete yet..)

--- 32-PROJECT-Texas-Hold-Em-Poker/poker/hand.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/poker/hand.py
@@ -1,6 +1,15 @@
-from poker.card import Card
-
-
 class Hand:
     def __init__(self, cards):
         self.cards = cards
+
+    def best_rank(self):
+        card_rank_counts = {}
+        for card in self.cards:
+            # Ace of Spades
+            card_rank_counts.setdefault(card.rank, 0)
+            card_rank_counts[card.rank] += 1
+
+        for rank, count in card_rank_counts.items():
+            if count == 2:
+                return "Pair"
+        return "High Card"
--- 32-PROJECT-Texas-Hold-Em-Poker/tests/test_hand.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/tests/test_hand.py
@@ -8,3 +8,19 @@ class handTest(unittest.TestCase):
         cards = [Card(rank="Ace", suit="Spades"), Card(rank="King", suit="Spades")]
         hand = Hand(cards=cards)
         self.assertEqual(hand.cards, cards)
+
+    def test_figures_put_high_card_is_best_rank(self):
+        cards = [
+            Card(rank="Ace", suit="Spades"),
+            Card(rank="King", suit="Spades"),
+        ]
+        hand = Hand(cards=cards)
+        self.assertEqual(hand.best_rank(), "High Card")
+
+    def test_figures_out_pair_is_best_rank(self):
+        cards = [
+            Card(rank="Ace", suit="Spades"),
+            Card(rank="Ace", suit="Hearts"),
+        ]
+        hand = Hand(cards=cards)
+        self.assertEqual(hand.best_rank(), "Pair")
