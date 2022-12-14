5beef02e394137a0cad36a619636201f56fff5b5
ledjo <ledjolleshaj@yahoo.com>
2022-12-14 19:14:50 +0100

    New hand rank and tests

--- 32-PROJECT-Texas-Hold-Em-Poker/poker/hand.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/poker/hand.py
@@ -3,13 +3,31 @@ class Hand:
         self.cards = cards
 
     def best_rank(self):
+
+        ranks_with_three_of_a_kind = self._ranks_with_count(3)
+
+        if len(ranks_with_three_of_a_kind) == 1:
+            return "Three of a Kind"
+
+        ranks_with_pairs = self._ranks_with_count(2)
+
+        if len(ranks_with_pairs) == 2:
+            return "Two Pair"
+        elif len(ranks_with_pairs) == 1:
+            return "Pair"
+        return "High Card"
+
+    def _ranks_with_count(self, count):
+        return {
+            rank
+            for rank, rank_count in self._card_rank_counts.items()
+            if rank_count == count
+        }
+
+    @property
+    def _card_rank_counts(self):
         card_rank_counts = {}
         for card in self.cards:
-            # Ace of Spades
             card_rank_counts.setdefault(card.rank, 0)
             card_rank_counts[card.rank] += 1
-
-        for rank, count in card_rank_counts.items():
-            if count == 2:
-                return "Pair"
-        return "High Card"
+        return card_rank_counts
--- 32-PROJECT-Texas-Hold-Em-Poker/tests/test_hand.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/tests/test_hand.py
@@ -24,3 +24,25 @@ class handTest(unittest.TestCase):
         ]
         hand = Hand(cards=cards)
         self.assertEqual(hand.best_rank(), "Pair")
+
+    def test_figures_out_two_pair_is_best_rank(self):
+        cards = [
+            Card(rank="Ace", suit="Spades"),
+            Card(rank="Ace", suit="Hearts"),
+            Card(rank="5", suit="Spades"),
+            Card(rank="King", suit="Spades"),
+            Card(rank="King", suit="Hearts"),
+        ]
+        hand = Hand(cards=cards)
+        self.assertEqual(hand.best_rank(), "Two Pair")
+
+    def test_figures_out_three_of_a_kind_is_best_rank(self):
+        cards = [
+            Card(rank="Ace", suit="Spades"),
+            Card(rank="Ace", suit="Hearts"),
+            Card(rank="Ace", suit="Diamonds"),
+            Card(rank="King", suit="Spades"),
+            Card(rank="King", suit="Hearts"),
+        ]
+        hand = Hand(cards=cards)
+        self.assertEqual(hand.best_rank(), "Three of a Kind")
