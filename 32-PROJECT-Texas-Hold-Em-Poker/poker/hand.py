21808b70be0647bcad7931d4805524e8cfa4975b
ledjo <ledjolleshaj@yahoo.com>
2023-01-02 17:29:29 +0100

    refactoring the hand class

--- 32-PROJECT-Texas-Hold-Em-Poker/poker/hand.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/poker/hand.py
@@ -26,25 +26,22 @@ class Hand:
     def __repr__(self):
         return (", ").join([str(card) for card in self.cards])
 
-    @property
-    def _rank_validations_from_best_to_worst(self):
-        return (
-            ("Royal Flush", RoyalFlushValidator(self.cards).is_valid),
-            ("Straight Flush", StraightFlushValidator(self.cards).is_valid),
-            ("Four of a Kind", FourOfAKindValidator(self.cards).is_valid),
-            ("Full House", FullHouseValidator(self.cards).is_valid),
-            ("Flush", FlushValidator(self.cards).is_valid),
-            ("Straight", StraightValidator(self.cards).is_valid),
-            ("Three of a Kind", ThreeOfAKindValidator(self.cards).is_valid),
-            ("Two Pair", TwoPairValidator(self.cards).is_valid),
-            ("Pair", PairValidator(self.cards).is_valid),
-            ("High Card", HighCardValidator(self.cards).is_valid),
-            ("No Cards", NoCardsValidator(self.cards).is_valid),
-        )
+    VALIDATORS = (
+        RoyalFlushValidator,
+        StraightFlushValidator,
+        FourOfAKindValidator,
+        FullHouseValidator,
+        FlushValidator,
+        StraightValidator,
+        ThreeOfAKindValidator,
+        TwoPairValidator,
+        PairValidator,
+        HighCardValidator,
+        NoCardsValidator,
+    )
 
     def best_rank(self):
-
-        for rank in self._rank_validations_from_best_to_worst:
-            name, validator_function = rank
-            if validator_function():
-                return name
+        for validator_klass in self.VALIDATORS:
+            validator = validator_klass(cards=self.cards)
+            if validator.is_valid():
+                return validator.name
--- 32-PROJECT-Texas-Hold-Em-Poker/tests/test_hand.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/tests/test_hand.py
@@ -1,6 +1,7 @@
 import unittest
 from poker.card import Card
 from poker.hand import Hand
+from poker.validators import PairValidator
 
 
 class handTest(unittest.TestCase):
@@ -25,3 +26,16 @@ class handTest(unittest.TestCase):
         hand = Hand()
         hand.add_cards(cards)
         self.assertEqual(hand.cards, [king_of_spades, ace_of_spades])
+
+    def test_interacts_with_validator_to_determine_winning_hand(self):
+        class HandWithOneValidator(Hand):
+            VALIDATORS = (PairValidator,)  # comma is important to create the tuple
+
+        hand = HandWithOneValidator()
+        hand.add_cards(
+            [
+                Card(rank="Ace", suit="Spades"),
+                Card(rank="Ace", suit="Clubs"),
+            ]
+        )
+        self.assertEqual(hand.best_rank(), "Pair")
