94279aa6e924c76eca6d37f8b2c425122a591f27
ledjo <ledjolleshaj@yahoo.com>
2023-01-03 21:12:30 +0100

    Use raw strings when u want to represent regex patterns

---
+++ 33-Regular-Expressions/review-of-raw-strings.py
@@ -0,0 +1,4 @@
+print("\tTake a look. There's a tab and line break\n right here.")
+
+# raw strings are done by adding an r before the string
+print(r"\tTake a look. There's a tab and line break\n right here.")
